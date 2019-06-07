eab7f123098cfe8b2f66eef94f789ea859d5d8b0
Golam Rabbani <golamrabbani268@gmail.com>
2019-06-07 21:20:02 +0600

    Update ex3.py

--- ex3.py
+++ ex3.py
@@ -1,12 +1,33 @@
+# Prints a string.
 print("I will now count my chickens:")
 
-print("Hens", 25+30/6)
-print("Roosters", 100-25*3%4)
+# Calculates and prints the number of hens and roosters.
+print("Hens", 25.0+30.0/6.0)
+print("Roosters", 100.0-25.0*3.0%4.0)
 
+# Prints a string.
 print("Now I will count the eggs:")
 
-print(3+2+1-5+4%2-1/4+6)
+# Calculates and prints the number of eggs.
+print(3.0+2.0+1.0-5.0+4.0%2.0-1.0/4.0+6.0)
 
+# Prints a string.
 print("Is it true that 3+2 < 5-7?")
 
+# Compares 3+2 and 5-7 using less than operator.
 print(3+2 < 5-7)
+
+# Calculates the values of 3+2 and 5-7.
+print("What is 3 + 2?", 3.0+2.0)
+print("What is 5 - 7?", 5.0-7.0)
+
+# Prints a string.
+print("Oh, that's why its False.")
+
+# Prints a string.
+print("How about some more.")
+
+# Comparing 5 and -2 using greater than, greater than or equal to & less than or equal to operators.
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
