ddb25f91c2d1f4a3c955cb24d73779f8bccf862f
Golam Rabbani <golamrabbani268@gmail.com>
2019-06-07 21:20:06 +0600

    Create sd3_3.py

---
+++ sd3_3.py
@@ -0,0 +1,8 @@
+print("How many seconds are there in a year?")
+print("Let's find out.")
+
+print("There are ", 60*60, " seconds in an hour.")
+print("24 hours in a day.")
+print("So ", 60*60*24, " seconds in a day!")
+print("There are 365 days in a year.")
+print("So there are ", 60*60*24*365, " seconds in a year!")
